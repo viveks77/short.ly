a2e0043313f7789cca1c707b0aacb5f4a9b88a4a
Vivek <viveksurya771@gmail.com>
2023-04-12 20:16:54 +0530

    Minor fix

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,6 +3,6 @@ export const ShortUrlConsts = {
 }
 
 export const BaseUrl = process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : `http://localhost:${process.env.PORT ?? 3000}`;
 
